1bae460fdd81eda16c0a01c8ad03cbaf441825dd
Полина Бегалиева <POLINA.BUTTERFLY@MAIL.RU>
2023-05-17 11:14:16 +0300

    Viewsets

--- cats/serializers.py
+++ cats/serializers.py
@@ -39,25 +39,26 @@ class CatSerializer(serializers.ModelSerializer):
         return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
-        # Если в исходном запросе не было поля achievements
         if 'achievements' not in self.initial_data:
-            # То создаём запись о котике без его достижений
             cat = Cat.objects.create(**validated_data)
             return cat
-
-        # Иначе делаем следующее:
-        # Уберём список достижений из словаря validated_data и сохраним его
         achievements = validated_data.pop('achievements')
-        # Сначала добавляем котика в БД
         cat = Cat.objects.create(**validated_data)
-        # А потом добавляем его достижения в БД
         for achievement in achievements:
             current_achievement, status = Achievement.objects.get_or_create(
                 **achievement)
-            # И связываем каждое достижение с этим котиком
             AchievementCat.objects.create(
                 achievement=current_achievement, cat=cat)
         return cat
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+    
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
+
 
 class OwnerSerializer(serializers.ModelSerializer):
     cats = serializers.StringRelatedField(many=True, read_only=True)
--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import mixins
+
+from django.shortcuts import get_object_or_404
 
 from .models import Cat, Owner
-from .serializers import CatSerializer, OwnerSerializer
+from .serializers import CatSerializer, OwnerSerializer, CatListSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
@@ -14,8 +17,34 @@ class CatViewSet(viewsets.ModelViewSet):
         cats = Cat.objects.filter(color='White')[:5]
         serializer = self.get_serializer(cats, many=True)
         return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = Cat.objects.all()
+        cat = get_object_or_404(queryset, pk=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CatListSerializer
+        return CatSerializer
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
+
+
+class UpdateDeleteViewSet(mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+                          viewsets.GenericViewSet):
+    pass
+
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    pass
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
